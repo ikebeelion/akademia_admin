ddeca6907074ead02c7b96d667355bc30ec3111a
ikebeelion <ianikwe@next-itservices.com>
2022-03-24 13:22:36 +0100

    Update Api.js

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,12 +1,9 @@
 import axios from 'axios'
 
 let Api = axios.create({
-    baseURL: "https://akademiaapi.herokuapp.com/api"
+    baseURL: "http://localhost:8000/api"
 })
 
-let user_token = $('meta[name="user-token]').attr('content')
-// Api.defaults.withCredentials = true
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.headers.common['Authorization'] = `Bear ${user_token}`;
+Api.defaults.withCredentials = true
 
 export default Api
