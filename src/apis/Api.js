381bca5b8c1baaa8032710d11988bbab6a157585
ikebeelion <ianikwe@next-itservices.com>
2022-04-01 18:18:55 +0100

    update

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let Api = axios.create({
-    baseURL: "https://akademiaapi.herokuapp.com/api"
+    baseURL: "https://akademiaadmin.herokuapp.com/api"
 })
 
 Api.defaults.withCredentials = true
