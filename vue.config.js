cc49f1cb34cd363a54189a040ddf8511ea231d38
ikebeelion <ianikwe@next-itservices.com>
2022-04-02 01:11:25 +0100

    push

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  
-})
+module.exports = {
+  devServer: {
+    proxy: "https://akademiaapi.herokuapp.com"
+  }
+};
