c638373963ee22855b12a86787f02f9b6e647bda
ikebeelion <ianikwe@next-itservices.com>
2022-04-02 01:59:24 +0100

    upc

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  devServer: {
-    proxy: "https://akademiaapi.herokuapp.com"
-  }
-};
+// module.exports = {
+//   devServer: {
+//     proxy: "https://akademiaapi.herokuapp.com"
+//   }
+// };
